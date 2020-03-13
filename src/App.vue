a3e0ebd8ba01b1c2e30131c9e64dbe31ea17c351
nnsense91 <HD-74@yandex.ru>
2020-03-13 18:06:35 +0500

    project init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+	.container
+		articleBlock
+		commentsBlock
+</template>
+
+<script>
+export default {
+	components: {
+		articleBlock: () => import ('./components/articleBlock'),
+		commentsBlock: () => import ('./components/commentsBlock'),
+	}
+}
+</script>
+
+<style lang="postcss">
+	.container {
+		width: 90%;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #3541c2
+	}
+</style>
---
+++ src/components/articleBlock.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+	article.article
+		h1.article__title Title
+		.article__date
+			span 01/01/2014
+		p.article__text 
+			| Lorem ipsum dolor sit amet consectetur adipiscing elit ultricies lacus tempor primis, cursus malesuada vel commodo mi nascetur conubia litora ligula nunc, eros aenean eleifend etiam condimentum at sociosqu penatibus nullam tristique. Scelerisque suscipit nostra risus pulvinar imperdiet phasellus fringilla integer cursus cum lectus porta, sit commodo est lacus platea quam ante hendrerit laoreet vulputate urna. Est mauris cursus ultricies mollis commodo lectus neque, malesuada arcu mus ligula maecenas sed, dis porttitor torquent scelerisque metus iaculis. Platea habitasse nunc eget fringilla scelerisque cursus erat massa sociis ipsum vel penatibus, nascetur egestas elementum interdum senectus aliquam phasellus sem inceptos placerat luctus eros magnis, aliquet tempus potenti viverra justo primis cum tristique nam vivamus neque. 
+			| Nullam habitasse malesuada gravida porta non litora habitant vel taciti, blandit elementum tellus molestie hac porttitor nisl sociosqu interdum, placerat urna dis conubia bibendum cras diam quis. Pharetra habitant in lobortis arcu turpis hac, est eleifend lectus aenean nascetur ornare, quisque duis dis enim hendrerit. Porta per vivamus semper risus interdum molestie malesuada dis erat, in venenatis consectetur lacinia vel ultrices quisque elit at et, consequat metus rutrum duis purus nisl sagittis pretium. Nec pellentesque fringilla montes quisque etiam et cum pretium fames, placerat tristique lectus aliquam aliquet ipsum purus laoreet mus, scelerisque auctor vestibulum aptent velit fermentum imperdiet gravida. Erat magnis massa nisl sodales consequat sollicitudin bibendum per facilisis quisque, elementum curabitur eros torquent scelerisque proin lobortis fusce habitasse.
+			br
+			br
+			| Justo dignissim quisque dui porttitor mauris suscipit magna venenatis, montes netus leo nullam integer semper ultrices sem cursus, luctus molestie risus dolor sit fames convallis. Ullamcorper malesuada natoque curabitur est praesent platea dis vel, dictumst scelerisque consequat ligula maecenas nisl mattis mi egestas, purus augue porttitor aliquet vulputate risus vivamus. Morbi nullam enim cum urna dolor mus, aliquam lorem fermentum aliquet potenti varius cubilia, suscipit risus velit iaculis ligula. Commodo eu dictumst mus placerat vel duis, ultricies at nec felis lectus rhoncus, curabitur ad habitasse ligula quis. Torquent mi purus ullamcorper semper sodales penatibus fermentum diam pellentesque velit, proin erat dis ornare nascetur nisl bibendum dui euismod, cursus molestie lacus felis luctus turpis adipiscing dolor neque.
+			| Facilisi pellentesque maecenas tortor commodo per lacinia felis elementum, ultricies metus viverra vitae hendrerit at torquent, class lacus sit quisque aliquam turpis primis. Porta penatibus mollis condimentum sed sem duis maecenas potenti vulputate nisi id, litora dignissim nostra nullam lacinia magnis aliquam etiam ac. Torquent ridiculus placerat arcu cum nulla mollis at ullamcorper, blandit mus tempor facilisis curae ac augue proin pretium, taciti scelerisque pellentesque lorem sollicitudin elit penatibus. Eleifend est lobortis pellentesque sodales amet enim arcu dignissim vivamus, ultrices fringilla rhoncus nascetur eu sit fames quisque, turpis senectus montes ridiculus purus egestas facilisis ligula molestie, viverra auctor scelerisque placerat proin tellus fermentum. Tellus senectus faucibus platea ridiculus maecenas pulvinar quisque ante scelerisque, leo diam lectus rutrum curabitur accumsan magna potenti molestie, cras dictum adipiscing posuere risus dignissim sociis iaculis.
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			
+		}
+	}
+}
+</script>
