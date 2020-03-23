535f7c92a7e7bcbde058cd2a9d3150b863f71ecc
nnsense91 <HD-74@yandex.ru>
2020-03-23 18:26:34 +0500

    Добавил легенду по markdown

--- src/components/commentsBlock.vue
+++ src/components/commentsBlock.vue
@@ -1,6 +1,12 @@
 <template lang="pug">
 	.comments
 		h2.comments__title Комментарии:
+		span(@mouseover="showLegendOn" @mouseleave="showLegendOff").comments__legend-link поддержка markdown
+		p(v-show="isLegendShow").comments__legend 
+			| #-заголовок h1 ... ###### - заголовок h6<br>
+			| > - цитата<br>
+			| * - пункт меню
+			| [link](https://google.com) - ссылка
 		commentsList(
 			:comments="comments"
 		)
@@ -14,7 +20,18 @@ export default {
 		commentsList: () => import ('./commentsList'),
 		commentsAddNew: () => import ('./commentsAddNew')
 	},
+	data() {
+		return {
+			isLegendShow: false
+		}
+	},
 	methods: {
+		showLegendOn() {
+			this.isLegendShow = true;
+		},
+		showLegendOff() {
+			this.isLegendShow = false;
+		}
 	},
 	computed: {
     ...mapState("comments", {
@@ -27,6 +44,20 @@ export default {
 
 <style lang="postcss" scoped>
  .comments {
-	width: 100%;
+		width: 100%;
+ }
+
+ .comments__legend-link {
+	 cursor: pointer;
+	 font-style: italic;
+	 color: #888;
+	 border-bottom: dotted;
+ }
+
+ .comments__legend {
+	 width: 50%;
+	 padding: 20px 0 20px 0;
+	 border-radius: 5px;
+	 background-color: #ccc;
  }
 </style>
