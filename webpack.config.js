0ee51f3105396b7135bf7b08de4ebd2e2ed3c23c
Ivan Egorov <HD-74@yandex.ru>
2020-03-23 21:48:55 +0500

    добавлен public path

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = (env, argv) => {
   const isProductionBuild = argv.mode === "production";
-  const publicPath = '/';
+  const publicPath = "https://nnsense91.github.io/commentsApp/dist/";
 
   const pcss = {
     test: /\.(p|post|)css$/,
